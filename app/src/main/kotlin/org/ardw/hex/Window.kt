4d0169003c9d672d23d424c495d8d7bac3a82cad
llschall <llschall@gmx.com>
2024-09-09 21:56:48 +0200

    [Keyboard] Add focus button

--- app/src/main/kotlin/org/ardw/hex/Window.kt
+++ app/src/main/kotlin/org/ardw/hex/Window.kt
@@ -2,6 +2,9 @@ package org.ardw.hex
 
 import org.llschall.ardwloop.serial.ArdwPortSelector
 import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
 import java.nio.file.Paths
 import javax.swing.*
 
@@ -13,7 +16,6 @@ class Window : JFrame() {
         pack()
         defaultCloseOperation = EXIT_ON_CLOSE
         isVisible = true
-        addKeyListener(Listener())
     }
 }
 
@@ -28,6 +30,9 @@ class Panel : JPanel() {
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
 
         val listBtn = JButton("list")
+
+
+        val focus = FocusMonitor()
 
         listBtn.addActionListener {
             val selector = ArdwPortSelector()
@@ -54,5 +59,32 @@ class Panel : JPanel() {
         add(JLabel("Current path is " + here.toAbsolutePath()), BorderLayout.NORTH)
         add(JScrollPane(table), BorderLayout.CENTER)
         add(listBtn, BorderLayout.SOUTH)
+        add(focus.button, BorderLayout.WEST)
+
+        focus.button.addKeyListener(Listener())
+        focus.button.requestFocus()
+    }
+}
+
+class FocusMonitor : FocusListener {
+
+    val button = JButton("Focus")
+
+    init {
+        button.isFocusable = true
+        button.isOpaque = true
+        button.background = Color.WHITE
+        button.addFocusListener(this)
+    }
+
+
+    override fun focusGained(e: FocusEvent?) {
+        button.background = Color.WHITE
+        button.revalidate()
+    }
+
+    override fun focusLost(e: FocusEvent?) {
+        button.background = Color.DARK_GRAY
+        button.revalidate()
     }
 }
